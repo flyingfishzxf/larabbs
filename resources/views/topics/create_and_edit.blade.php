f79541438ed3164c43102c9c38ea3c135715af74
flyingfishzxf <flyingfishzxf@gmail.com>
2018-01-28 12:50:41 +0000

    WYSIWYG Editor

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -60,4 +60,23 @@
     </div>
 </div>
 
-@endsection
+@stop
+
+@section('styles')
+    <link rel="stylesheet" href="{{ asset('css/simditor.css') }}" type="text/css">
+@stop
+
+@section('scripts')
+    <script src="{{ asset('js/module.js') }}"></script>
+    <script src="{{ asset('js/hotkeys.js') }}"></script>
+    <script src="{{ asset('js/uploader.js') }}"></script>
+    <script src="{{ asset('js/simditor.js') }}"></script>
+
+    <script>
+        $(document).ready(function () {
+            var editor = new Simditor({
+                textarea: $('#editor')
+            });
+        });
+    </script>
+@stop
