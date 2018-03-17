975f5904434875525df168461a17836133542c47
flyingfishzxf <flyingfish@gmail.com>
2018-03-17 01:58:42 +0000

    add captcha

--- config/api.php
+++ config/api.php
@@ -233,8 +233,8 @@ return [
     'rate_limits' => [
         //
         'access' => [
-            'expires' => env('RATE_LIMITS_EXPIRES', 1);
-            'limit' => env('RATE_LIMITS', 60);
+            'expires' => env('RATE_LIMITS_EXPIRES', 1),
+            'limit' => env('RATE_LIMITS', 60),
         ],
         'sign' => [
             'expires' => env('SIGN_RATE_LIMITS_EXPIRES', 1),
